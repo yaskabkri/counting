7d5cbbb83ec9936d835e05631c86b80e6a9f546a
yaskabkri <yaskabkri@gmail.com>
2024-02-04 22:00:49 +0300

    anyway

--- myhsb/settings.py
+++ myhsb/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'prd',
     'staf',
+    'crispy_forms'
+    
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'  # Use 'bootstrap', 'bootstrap4', 'uni-form', or 'foundation-5'
+
